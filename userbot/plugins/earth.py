8865a8fd73d26cf166c0362d9d554a3d1c984fb9
Aryan Kumar <83180468+Aruack@users.noreply.github.com>
2022-02-09 01:55:02 +0530

    Add files via upload

---
+++ userbot/plugins/earth.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2022 ARUACK.
+
+
+from telethon import events
+import asyncio
+from collections import deque
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern="earth"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+	for _ in range(48):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
